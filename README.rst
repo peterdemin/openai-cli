b48f687538e8f5df91612a17456973033dc8e193
Peter Demin <peter.demin@toptal.com>
2022-12-03 18:28:27 -0500

    First working version

--- README.rst
+++ README.rst
@@ -1,1 +1,56 @@
-# OpenAI command-line interface
+OpenAI command-line client
+==========================
+
+Installation
+------------
+
+To install OpenAI CLI in Python virtual environment, run::
+
+    pip install openai-cli
+
+Token authentication
+--------------------
+
+OpenAI API requires authentication token, which can be obtained on this page:
+https://beta.openai.com/account/api-keys
+
+Provide token to the CLI either through a command-line argument (``-t/--token <TOKEN>``)
+or through an environment variable (``OPENAI_API_TOKEN``).
+
+Usage
+-----
+
+Currently only text completion API is supported.
+
+Example usage::
+
+    $ echo "Are cats faster than dogs?" | openai complete -
+    It depends on the breed of the cat and dog. Generally,
+    cats are faster than dogs over short distances,
+    but dogs are better at sustained running.
+
+Interactive mode supported (Press Ctrl+C to exit)::
+
+    $ openai repl
+    Prompt: Can generative AI replace humans?
+
+    No, generative AI cannot replace humans.
+    While generative AI can be used to automate certain tasks,
+    it cannot replace the creativity, intuition, and problem-solving
+    skills that humans possess.
+    Generative AI can be used to supplement human efforts,
+    but it cannot replace them.
+
+    Prompt: ^C
+
+Run without arguments to get a short help message::
+
+    $ openai
+    Usage: openai [OPTIONS] COMMAND [ARGS]...
+
+    Options:
+      --help  Show this message and exit.
+
+    Commands:
+      complete  Return OpenAI completion for a prompt from SOURCE.
+      repl      Start interactive shell session for OpenAI completion API.
--- src/openai_cli/cli.py
+++ src/openai_cli/cli.py
@@ -11,76 +11,63 @@ def cli():
 
 
 @cli.command()
-@click.argument('source', type=click.File('rt', encoding='utf-8'))
-@click.option('-t', '--token', default='', help='OpenAI API token')
+@click.argument("source", type=click.File("rt", encoding="utf-8"))
+@click.option("-t", "--token", default="", help="OpenAI API token")
 def complete(source: io.TextIOWrapper, token: str) -> None:
-    """Execute CODE on SOURCE."""
-    token = token or os.environ.get('OPENAI_API_TOKEN', '')
+    """Return OpenAI completion for a prompt from SOURCE."""
+    token = token or os.environ.get("OPENAI_API_TOKEN", "")
     if not token:
         click.exceptions.MissingParameter(
-            'Either --token option or OPENAI_API_TOKEN '
-            'environment variable must be provided',
+            "Either --token option or OPENAI_API_TOKEN "
+            "environment variable must be provided",
         )
-    client = ChatGptClient(token)
+    client = CompletionClient(token)
     prompt = source.read()
     result = client.generate_response(prompt)
     click.echo(result)
 
 
 @cli.command()
-@click.option('-t', '--token', default='', help='OpenAI API token')
+@click.option("-t", "--token", default="", help="OpenAI API token")
 def repl(token: str) -> None:
-    """Execute CODE on SOURCE."""
-    token = token or os.environ.get('OPENAI_API_TOKEN', '')
+    """Start interactive shell session for OpenAI completion API."""
+    token = token or os.environ.get("OPENAI_API_TOKEN", "")
     if not token:
         click.exceptions.MissingParameter(
-            'Either --token option or OPENAI_API_TOKEN '
-            'environment variable must be provided',
+            "Either --token option or OPENAI_API_TOKEN "
+            "environment variable must be provided",
         )
-    client = ChatGptClient(token)
+    client = CompletionClient(token)
     while True:
-        print(client.generate_response(input('Enter prompt: ')))
+        print(client.generate_response(input("Prompt: ")))
 
 
-class ChatGptClient:
-    def __init__(self, api_key: str):
-        # Set the API key
-        self.api_key = api_key
+class CompletionClient:
+    API_URL = "https://api.openai.com/v1/completions"
+    MAX_TOKENS = 1000
+    TEMPERATURE = 0.1
 
-        # Set the API endpoint URL
-        self.api_url = 'https://api.openai.com/v1/completions'
-
-    def generate_response(self, prompt: str, model: str = 'text-davinci-003') -> str:
-        # Set the request headers
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.api_key}',
+    def __init__(self, api_key: str) -> None:
+        self._headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}",
         }
 
-        # Set the request payload
-        payload = {
-            'prompt': prompt,
-            'model': model,
-            'temperature': 0.5,
-            'max_tokens': 100,
-        }
-
-        # Send the request to the API endpoint
+    def generate_response(self, prompt: str, model: str = "text-davinci-003") -> str:
         response = requests.post(
-            self.api_url,
-            headers=headers,
-            json=payload,
+            self.API_URL,
+            headers=self._headers,
+            json={
+                "prompt": prompt,
+                "model": model,
+                "max_tokens": self.MAX_TOKENS,
+                "temperature": self.TEMPERATURE,
+            },
             timeout=60,
         )
-
-        # Check the response status code
-        if response.status_code != 200:
-            # Handle the error here
-            pass
-
-        # Get the response text
-        return response.json()['choices'][0]['text']
+        response.raise_for_status()
+        return response.json()["choices"][0]["text"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
