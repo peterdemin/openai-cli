f4c1fd493c2d46142210c455243b0e4772e4fde3
Mustafa Tevfik Kadan (SSH User) <mtevfik41@gmail.com>
2024-08-28 17:33:04 +0300

    fix merge conflict

--- docs/conf.py
+++ docs/conf.py
@@ -9,28 +9,6 @@ extensions = [
 master_doc = "index"
 html_theme = "furo"
 
-# Link dates and other references in the changelog
-extensions += ['rst.linker']
-link_files = {
-    '../CHANGES.rst': dict(
-        using=dict(GH='https://github.com'),
-        replace=[
-            dict(
-                pattern=r'(Issue #|\B#)(?P<issue>\d+)',
-                url='{package_url}/issues/{issue}',
-            ),
-            dict(
-                pattern=r'(?m:^((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n)',
-                with_scm='{text}\n{rev[timestamp]:%d %b %Y}\n',
-            ),
-            dict(
-                pattern=r'PEP[- ](?P<pep_number>\d+)',
-                url='https://peps.python.org/pep-{pep_number:0>4}/',
-            ),
-        ],
-    )
-}
-
 # Be strict about any broken references
 nitpicky = True
 
